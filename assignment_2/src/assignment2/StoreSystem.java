1aae64187a5fd6f65bffc52583a0fe1b7bf80542
j-dorty <jack.doherty32@mail.dcu.ie>
2021-04-12 14:33:23 +0100

    more stuff

--- assignment_2/src/assignment2/StoreSystem.java
+++ assignment_2/src/assignment2/StoreSystem.java
@@ -4,7 +4,9 @@ import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.List;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.rmi.server.UnicastRemoteObject;
+import java.text.SimpleDateFormat;
 import java.rmi.*;
 import java.io.*;
 import java.util.Scanner;
@@ -14,6 +16,7 @@ import java.util.Arrays;
 import java.util.Set;
 import java.io.Serializable;
 import java.util.*;
+import java.time.LocalDate;  
 import java.util.Queue;
 import java.io.File ;
 
@@ -37,24 +40,9 @@ public class StoreSystem extends UnicastRemoteObject implements StoreInterface, 
   public StoreSystem() throws RemoteException, Exception {
     super();
     file = new file(fileName);
-    br = new BufferedReader(new FileReader(file));
   }
 
-
-
-  public  String printFile() throws RemoteException ,  Exception
-  {
-      System.out.println("PrintFile request");
-
-      String st ;
-      String f = "" ;
-      while ((st = br.readLine()) != null){
-          f = f + st ;
-      }
-      return f ;
-  }
-
-  public synchronized ArrayList<String> placeOrder(String order) throws RemoteException, Exception {
+  public synchronized void placeOrder(String order) throws RemoteException, Exception {
     // order = "Game Boy 500 12/05/2021 12:00"
     String[] orderAsList = order.split(" "); // ["game", "boy", "500",...]
     String orderedProduct = String.join(" ", Arrays.copyOfRange(orderAsList, 0, orderAsList.length - 3)); //order[:-3]
@@ -90,6 +78,7 @@ public class StoreSystem extends UnicastRemoteObject implements StoreInterface, 
     } catch(FileNotFoundException e){
       e.printStackTrace();
     }
+    bufferedReader.close();
 
   }
 
@@ -110,15 +99,98 @@ public class StoreSystem extends UnicastRemoteObject implements StoreInterface, 
     } catch(FileNotFoundException e) {
       e.printStackTrace();
     }
+    bufferedReader.close();
   }
 
-  public synchronized void checkProductAvailability(String productName, String checkDate) {
+  public synchronized void checkProductAvailability(String productName, LocalDate checkDate) {
+    LocalDate currentDate = LocalDate.now(); //2017-01-03
+    int daysBetweenDates = currentDate.until(checkDate, ChronoUnit.DAYS);
+    int numberOfRestocks = daysBetweenDates % 30; // 1
+    int daysRemaining = daysBetweenDates / 30; // 10
+
+    FileReader fileReader = new FileReader("store_inventory.txt");
+    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+    String line = bufferedReader.readLine();
+
+    String productInfo;
+    try {
+      while(line != null) {
+        inventoryEntry = line.split(" ");
+
+        inventoryProduct = String.join(" ", Arrays.copyOfRange(inventoryEntry, 0, inventoryEntry.length - 3));
+        if(inventoryProduct == productName) {
+          productInfo = inventoryEntry;
+          break;
+        }
+      }
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+
+    String restockQuanity = inventoryEntry[inventoryEntry.length - 1];
+    String restockDate = inventoryEntry[inventoryEntry.length - 2];
+    String currentQuantity = inventoryEntry[inventoryEntry.length - 3];
+
+    currentDay = currentDate.getDayOfMonth();
+    if(currentDay < parseInt(restockDate)) {
+      checkDay = currentDay + daysRemaining;
+      if(checkDay > parseInt(restockDate)) {
+        numberOfRestocks ++;
+      }
+    }
+
+    int futureQuantityWithNoOrders = parseInt(currentQuantity) + (parseInt(restockQuanity) * numberofRestocks);
     
+    FileReader ordferFileReader = new FileReader("current_orders.txt");
+    BufferedReader orderBufferedReader = new BufferedReader(fileReader);
+
+    String currentOrder = orderBufferedReader.readLine();
+
   }
+
+
+  // userId OrderID orderredproduct qauntity date ....
+  // 1 35 game boy....
+  public synchronized void cancelOrder(String userId, String orderIdTobeCancelled) {
+    ArrayList<String> ordersNotToBecCancelled;
+
+    FileReader fileReader = new FileReader("current_orders.txt");
+    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+    String currentOrder = bufferedReader.readLine();
+
+    try {
+      while(currentOrder != null) {
+        currentOrderList = line.split(" ");
+
+        customerId = currentOrderList[0];
+        orderId = currentOrderList[1];
+        if(!(customerId == userId && orderId == orderIdTobeCancelled)) {
+          ordersNotToBecCancelled.add(currentOrder);
+        }
+
+      }
+
+      FileWriter fileWriter = new FileWriter("filepath.txt", false);
+
+      for(String order : ordersNotToBecCancelled) {
+
+        fileWriter.write(order);
+      }
+      fileWriter.close();
+      bufferedReader.close();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  } 
+
 
   private void confirmOrder(String orderedProduct, String orderedQuantity, String orderDate, String orderTime) {
     try {
-      FileWriter fileWriter = new FileWriter("current_orders.txt");
+      FileWriter fileWriter = new FileWriter("current_orders.txt", true);
       // 1 Game Boy 500 12/5/2021 12:00
       fileWriter.write("1 " + orderedProduct + " " + orderedQuantity + " " + orderDate + " " + orderTime);
       fileWriter.close();
